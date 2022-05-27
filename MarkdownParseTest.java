a7dbe1b95bbe303631b15c3c526d6308b1fd7d07
asaini27 <asaini@ucsd.edu>
2022-05-26 17:57:49 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -9,6 +9,11 @@ import java.util.List;
 public class MarkdownParseTest {
 
     @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
     public void testFile1() throws IOException {
         String contents= Files.readString(Path.of("./test-file.md"));
         List<String> expect = List.of("https://something.com", "some-thing.html");
